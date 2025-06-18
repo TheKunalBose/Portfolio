69249005a818ace5c00174f46d5454bfe5c65254
kunal <kunalvon3002@gmail.com>
2025-06-18 14:57:17 +0530

    mail update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "kbose0204@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
